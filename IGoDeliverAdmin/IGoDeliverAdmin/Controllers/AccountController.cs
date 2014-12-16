7f04b2b1d15142285cd9dd0b65e619f520f9158a
Amos Zhou <amoszhou@gmail.com>
2014-12-16 22:37:27 +1100

    Added picture upload features

--- IGoDeliverAdmin/IGoDeliverAdmin/Controllers/AccountController.cs
+++ IGoDeliverAdmin/IGoDeliverAdmin/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using DotNetOpenAuth.AspNet;
+using IGoDeliverAdmin.Entity;
 using Microsoft.Web.WebPages.OAuth;
 using WebMatrix.WebData;
 using IGoDeliverAdmin.Models;
--- IGoDeliverAdmin/IGoDeliverAdmin/Controllers/DishController.cs
+++ IGoDeliverAdmin/IGoDeliverAdmin/Controllers/DishController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.WebPages;
+using IGoDeliverAdmin.Entity;
 using IGoDeliverAdmin.Models;
 
 namespace IGoDeliverAdmin.Controllers
@@ -117,6 +119,72 @@ namespace IGoDeliverAdmin.Controllers
             db.Dishes.Remove(dish);
             db.SaveChanges();
             return RedirectToAction("Edit", "Restaurant", new { id = dish.RestaurantId });
+        }
+
+        [HttpPost]
+        public ActionResult SaveFile(HttpPostedFileBase fileToUpload, int dishId)
+        {
+            if (fileToUpload != null && fileToUpload.ContentLength > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(fileToUpload.FileName);
+
+                var path = Path.Combine(Server.MapPath("~/Images/"), fileName);
+                fileToUpload.SaveAs(path);
+
+
+                var p = new PictureOfDish() { Path = "/Images/" + fileName };
+                db.PictureOfDishes.Add(p);
+                db.SaveChanges();
+                //var ip = new DishPicture{ DishId = dishId, PictureId = p.Id, IsDefault = false };
+                //db.DishPictures.Add(ip);
+                var ip = new DishPicture { DishId = dishId, PictureId = p.Id, IsDefault = false };
+                db.DishPictures.Add(ip);
+                db.SaveChanges();
+            }
+            return RedirectToAction("ManagePicture", new { id = dishId });
+        }
+
+
+        [HttpGet]
+        public ActionResult ManagePicture(int id = 0)
+        {
+            Dish item = db.Dishes.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        [HttpGet]
+        public ActionResult DeletePicForItem(int itemid, int picid)
+        {
+            DishPicture itemPic = db.DishPictures.FirstOrDefault(p => p.PictureId == picid && p.DishId == itemid);
+            if (itemPic == null)
+            {
+                return HttpNotFound();
+            }
+            db.DishPictures.Remove(itemPic);
+            db.SaveChanges();
+
+            return RedirectToAction("ManagePicture", new { id = itemid });
+        }
+
+        [HttpGet]
+        public ActionResult SetDefaultPic(int itemid, int picid)
+        {
+            DishPicture itemPic = db.DishPictures.FirstOrDefault(p => p.PictureId == picid && p.DishId == itemid);
+            if (itemPic == null)
+            {
+                return HttpNotFound();
+            }
+            foreach (var p in db.DishPictures.Where(p => p.DishId == itemid))
+            {
+                p.IsDefault = false;
+            }
+            itemPic.IsDefault = true;
+            db.SaveChanges();
+            return RedirectToAction("ManagePicture", new { id = itemid });
         }
 
         protected override void Dispose(bool disposing)
